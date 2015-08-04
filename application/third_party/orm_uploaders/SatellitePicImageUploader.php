141a75e8a545ae42056753253a01e7a32f97f1d6
OA Wu <comdan66@gmail.com>
2015-08-05 01:40:20 +0800

    Fix pic bug.

--- application/third_party/orm_uploaders/SatellitePicImageUploader.php
+++ application/third_party/orm_uploaders/SatellitePicImageUploader.php
@@ -14,6 +14,7 @@ class SatellitePicImageUploader extends OrmImageUploader {
         '100x100c' => array ('adaptiveResizeQuadrant', 100, 100, 'c'),
         '200x200c' => array ('adaptiveResizeQuadrant', 200, 200, 'c'),
         '400x400c' => array ('adaptiveResizeQuadrant', 400, 400, 'c'),
+        '800x800c' => array ('adaptiveResizeQuadrant', 800, 800, 'c'),
       );
   }
 }
